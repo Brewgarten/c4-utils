d73c58ac38e8b85b8c79132ca24a53849bc76c4d
Martin Kuehnhausen <martin.kuehnhausen@us.ibm.com>
2016-01-10 21:52:23 -0600

    Added pytest and coverage integration (closes #1)

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+import sys
+
 from setuptools import setup, find_packages
 
 import versioneer
+
+
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setup(
     name = "c4-utils",
@@ -8,6 +14,8 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     packages = find_packages(),
     install_requires = [],
+    setup_requires=[] + pytest_runner,
+    tests_require=["pytest", "pytest-cov"],
     author = "IBM",
     author_email = "",
     description = "This is a collection of Python utility modules for project C4",
