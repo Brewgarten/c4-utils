36e6945c069a898984dad696fb7cc8fe81b6e05b
Martin Kuehnhausen <martin.kuehnhausen@us.ibm.com>
2017-04-26 11:11:57 -0500

    Fix hard-coded paths in test cases (#14)

--- tests/test_command.py
+++ tests/test_command.py
@@ -3,7 +3,7 @@ import pytest
 from c4.utils.command import execute, CommandException
 
 def test_execute_withOutput():
-    response = execute(["ls", "/tmp"])
+    response = execute(["ls", "/"])
     assert response is not None
 
 def test_execute_withoutOutput():
@@ -14,7 +14,7 @@ def test_execute_fail():
     try:
         execute(["ls", "/test"], "/test does not exist")
     except CommandException as e:
-        assert "ls: cannot access /test: No such file or directory" == e.error
+        assert "No such file or directory" in e.error
 
 def test_execute_exception():
     with pytest.raises(CommandException):
@@ -24,4 +24,4 @@ def test_execute_unknownCommand():
     try:
         execute(["testcommand"])
     except CommandException as e:
-        assert "[Errno 2] No such file or directory" == e.error
+        assert "No such file or directory" in e.error
